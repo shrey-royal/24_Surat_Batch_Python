4310e8eb7802098a0665f3b5aa8244bc2b52d976
Shrey K <srkkadia@gmail.com>
2024-09-10 11:14:42 +0530

    First

---
+++ Basics/First.py
@@ -0,0 +1,16 @@
+# single line comment
+'''
+multi
+line
+comment
+'''
+"""
+multi
+line
+comment
+"""
+'''
+print function:
+syntax:
+    print(*objects, sep=' ', end="\n", file=file.sysout, flush=False) -> None
+'''
