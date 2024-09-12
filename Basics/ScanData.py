d3fc8bb974154c87df64ea63b8b8fca845bed4c8
Shrey K <srkkadia@gmail.com>
2024-09-12 11:59:39 +0530

    operators, scandata, string

---
+++ Basics/ScanData.py
@@ -0,0 +1,10 @@
+'''
+> input fn is used to scan data from the user (default input type is str)
+syntax:
+    var = input("prompt to user") # type str
+'''
+
+# var = input("Enter str: ")
+# var = int(input("Enter int: "))
+var = bool(input("Enter bool: "))
+print(var, type(var))
---
+++ Basics/String.py
@@ -0,0 +1,36 @@
+# String declaration
+
+# name = "Jay Vaghasiya"
+# message = "Hello, Vaghasiya"
+
+# print(name)
+# print(message)
+
+# f_ormatted strings (fstring)
+
+# message = f"Good Morning, {name}"
+# print(message)
+
+# greetings = 'Good' 'Morning!' 'Naimishji'   # concatenation
+# print(greetings)
+
+# greeting = 'Good '
+# time = 'Evening'
+
+# greeting = greeting + time + '!'
+# print(greeting)
+
+# -----------------------------------------------------------
+
+# Accessing string element
+
+str = None
+
+str = 'Python String'
+print(str[0])
+print(str[12])
+
+print(len(str))
+
+print(str[-1])
+print(str[12])
