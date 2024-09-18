90994ca049e79c598bc1acfd6117a299ad753907
Shrey K <srkkadia@gmail.com>
2024-09-18 10:55:04 +0530

    String Methods (e)

---
+++ Basics/String_Methods.py
@@ -0,0 +1,21 @@
+# text = "hello world"
+# print(text.capitalize())
+
+# text = "pYtHoN pRoGRamMInG"
+# print(text.capitalize())
+
+# text = "HELLO WORLD"
+# print(text.casefold())
+
+# text = "Zafar Sir"
+# print(f"'{text.center(50, '_')}'")
+
+# text = "hello world"
+# print(text.count('l', 2, 5))
+
+# text = "cafē"
+# print(text.encode(encoding='ascii', errors='ignore'))
+
+# text = "python programming is fun"
+# print(text.endswith('is fun'))
+# print("Hello\tWorld abc".expandtabs(8))
