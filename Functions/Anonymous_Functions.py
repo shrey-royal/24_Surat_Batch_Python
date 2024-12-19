7dd86c83ae08a03e02b49024326dad161d39a952
Shrey K <srkkadia@gmail.com>
2024-12-19 11:39:26 +0530

    Lambda Functions

---
+++ Functions/Anonymous_Functions.py
@@ -0,0 +1,33 @@
+# Anonymous Functions in Python
+
+# lambda parameters: expression => syntax
+
+# variable = lambda parameter: expression   => function definition
+# variable(parameter) => function calling
+
+# Normal function
+# def square(x):
+#     return x*x
+
+# ans = square(4)
+# print(ans)
+
+# lambda function
+# square2 = lambda x: x*x
+# print(square2(6))
+
+
+# lambda with multiple parameters
+# add = lambda a=23, b=34: a+b
+# print(add(2, 3))
+# print(add())
+
+
+# parameterless lambda functions
+
+# greet = lambda : "Hello!"
+# print(greet())
+
+# inline lambda
+# print((lambda x : x*x*x)(3))
+# -------------------------------------------------------------------------------
--- Functions/func.py
+++ Functions/func.py
@@ -1,48 +1,103 @@
-"""
-function: A named codeblock who performs operations and return the output(optional).
+'''
+Functions --> Assistants
+
+=> A function is a named code block that performs a job or return a value
 
 syntax:
 def function_name(argument/s):
-    body of the function
-"""
+    //body of the function
 
-def greet():
-    print("Hello, there!")
+'''
 
-# greet()
+# import datetime as dt
 
-def add(x, y):
-    # print(x+y)
-    return x+y
+# def greet():
+#     now = dt.datetime.now()
+#     current_hour = now.hour
 
-# print(add(2, 33))
+#     if 5 <= current_hour < 12:
+#         greeting = "Good morning!"
+#     elif 12 <= current_hour < 17:
+#         greeting = "Good afternoon!"
+#     elif 17 <= current_hour < 21:
+#         greeting = "Good evening!"
+#     else:
+#         greeting = "Good night!"
 
-def palindrome(n):
-    s = str(n)
-    return s == s[::-1]
+#     return greeting
+
+# print(greet())
+######################################################################
+
+# Doc Strings (Documentation String)
+
+# def print(message):
+#     """This is Doc String"""
+#     print(message)
+
+# print()
+######################################################################
+
+
+# def greet(name):
+#     print(f'Hi, {name}')
+
+# greet(input("Enter your name: "))
+
+# def greet(name):
+#     return f'Hi, {name}'
+
+# res = greet(input("Enter your name: "))
+# print(res)
+# print(greet(input("Enter your name: ")))
+######################################################################
+
+# def addition(a, b):
+#     return a+b
+
+# print(addition(2, 3))
+
+
+# Default parameters
+
+# def addition(a, b=0):
+#     print(f"{a} + {b} = {a+b}")
+
+# addition(34, 34)
+# addition(34)
+
+# smol praktis
+# Problem Statement: Create a function 'greet_user' that takes two parameters: 'name' (a string) and 'greeting' (a string). The greeting parameter should have a default value of "Hello". The function should print a message in the format: "greeting, name!". If no greeting is provided, it should use the default value.
+
+# def greet_user(name,greeting="hello"):
+#     print(f'"{greeting}, {name}!"')
+
+# greet_user('baburao','good afternoon')
+# greet_user('baburao')
+######################################################################
+
+# keyword arguments
+
+# def addition(a: int | None = 0, b=0):
+#     print(f"{a} + {b} = {a+b}")
+
+# addition(b=23, a=1)
+# addition(b=23)
+
+
+# print("this", "is", "a", "firecracker!", end='___')
+######################################################################
+
+# Recursion Function
+
+def factorial(num=0):
+    # base condition
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
     
+    # recursion
+    return num * factorial(num-1)
 
-print(palindrome("aacdcaa"))
-print(palindrome(12321))
-
-# check armstrong number
-
-# 15 - 26
-# 153 - 1 + 125 + 27 = 153
-
-def is_armstrong(number):
-    num_str = str(number)
-    num_digits = len(num_str)
-    total = sum(int(digit) ** num_digits for digit in num_str)
-    
-    return total == number
-
-num = int(input("Enter a number: "))
-if is_armstrong(num):
-    print(f"{num} is an Armstrong number.")
-else:
-    print(f"{num} is not an Armstrong number.")
-
-
-# convert decimal to binary
-# 8 -> 1000
+print(factorial(5))
