dcc810a0e9ad3c1a758d7a4d4478af145e5145df
Shrey K <srkkadia@gmail.com>
2025-01-08 11:53:00 +0530

    Modules

---
+++ Modules/funcs.py
@@ -0,0 +1,21 @@
+def addition(a, b):
+    return a+b
+
+def subtraction(a, b):
+    return a-b
+    
+def multiplication(a, b):
+    return a*b
+
+def division(a, b):
+    return a/b
+
+def main():
+    print(addition(10, 2))
+    print(subtraction(10, 2))
+    print(multiplication(10, 2))
+    print(division(10, 2))
+
+
+if __name__ == "__main__":  # main block
+    main()
---
+++ Modules/modules.py
@@ -0,0 +1,3 @@
+import funcs as f
+
+print(f.addition(20, 3))
